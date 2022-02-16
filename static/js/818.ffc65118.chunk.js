5e269a64b842461d6230930f42f41e89489d4a80
veriano <86019033+veriano@users.noreply.github.com>
2022-02-16 12:31:50 +0000

    Deploying to gh-pages from @ veriano/goit-react-hw-05-movies@2ff2a84bd9cd2e334939765b96fb9f70c69b312e 🚀

--- static/js/818.ffc65118.chunk.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunkgoit_react_hw_05_movies=self.webpackChunkgoit_react_hw_05_movies||[]).push([[818],{432:function(){},818:function(e,t,n){"use strict";n.r(t),n.d(t,{default:function(){return c.a}});var u=n(432),c=n.n(u)}}]);
