6997174aed394c63332a01e37453b3de0fef281b
veriano <veriano@ukr.net>
2022-02-18 12:51:36 +0200

    update

--- src/components/MovieDetailsPage/MovieDetailsPage.js
+++ src/components/MovieDetailsPage/MovieDetailsPage.js
@@ -40,20 +40,22 @@ const MovieDetailsPage = () => {
             {movie &&
                 <>
                     <img src='*' alt={ movie.title } />
-                    <div className={ s.infoOfMovie }>
-                        <h1>{ movie.title }({ movie.release_date })</h1>
-                        <h2>Overview</h2>
-                        <p>{ movie.overview }</p>
-                        <h3>Genres</h3>
+                    <div className={s.infoOfMovie}>
+                    
+                    <h1><b>{movie.title}({movie.release_date})</b></h1>
+                    <p className={ s.text }>User Score: {movie.vote_average * 10}%</p>
+                    <h2>Overview</h2>
+                    <p className={ s.text }>{ movie.overview }</p>
+                    <h3>Genres</h3>
                        
                     </div>
                 </>
             }
             <>
             {movie &&
-            <ul className={s.listDetails}>
-                <li><Link to={`/movies/${ movie.id }/cast`} className={s.link}>Cast</Link></li>
-                <li><Link to={`/movies/${ movie.id }/reviews`} className={s.link}>Reviews</Link></li>
+            <ul className={ s.listDetails }>
+                <li><Link to={`/movies/${ movie.id }/cast`} className={ s.link }>Cast</Link></li>
+                <li><Link to={`/movies/${ movie.id }/reviews`} className={ s.link }>Reviews</Link></li>
             </ul>}
             </>
         <Routes>
