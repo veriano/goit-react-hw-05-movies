553e82e2e1f2e9eaa0bc7876f171065bb28a9be8
veriano <86019033+veriano@users.noreply.github.com>
2022-02-20 21:05:21 +0200

    update

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,17 @@
-// import { lazy } from 'react';
+import { lazy, Suspense } from 'react';
 import { Route, Routes, Outlet } from 'react-router-dom';
 import HomePage from './HomePage';
 import AppBar from './AppBar';
 import NoMatch from './NoMatch';
-import MoviesPage from'./MoviesPage';
-import MovieDetailsPage from './MovieDetailsPage';
+const MoviesPage = lazy(() => import('./MoviesPage'));
+const MovieDetailsPage = lazy(() => import('./MovieDetailsPage'));
 
 
 function App () {
   return (
     <div>
       <AppBar />
+      <Suspense fallback={<h1>...Загружаем</h1>}>
       <Routes>
         <Route path='/goit-react-hw-05-movies' index element={ <HomePage /> } />
         
@@ -22,7 +23,7 @@ function App () {
 
         <Route element={ <Outlet /> } />
       </Routes>
-      
+      </Suspense>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,22 +1,23 @@
-import React from 'react';
+import React, { lazy, Suspense } from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import  App  from './components/App';
 import './index.css';
-import Cast from './routes/Cast';
-import Reviews from './routes/Reviews';
+const Cast = lazy(() => import('./routes/Cast'));
+const Reviews = lazy(() => import('./routes/Reviews'));
 
 
 ReactDOM.render(
   <React.StrictMode>
     <BrowserRouter>
         <App />
+      <Suspense fallback={<div>...Загружаем</div>}>
       <Routes>
         <Route path='/movies/:movieId/cast' element={ <Cast /> } />
         
         <Route path='/movies/:movieId/reviews' element={ <Reviews /> } />
       </Routes>
-    
+    </Suspense>
   </BrowserRouter>
   </React.StrictMode>,
   document.getElementById('root')
